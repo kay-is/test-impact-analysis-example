39e406b8cc6a89f8e2ac75cc39f744d1f9a94332
Kay Plößer <kay-is@users.noreply.github.com>
2021-11-17 22:03:31 +0000

    Update test

--- scripts/run-tia.js
+++ scripts/run-tia.js
@@ -8,14 +8,18 @@ async function main() {
     /src\/.*.js/gi.test(file)
   )
 
-  console.log(changedSourceFiles)
-
   if (changedSourceFiles.length < 1)
     return console.log("No JS files in src directory changed. Aborted.")
 
   for (const sourceFile of changedSourceFiles) {
+    console.log("Finding tests for source file: ", sourceFile)
     for (const testFile of Object.keys(tia)) {
-      if (tia[testFile].includes(sourceFile)) requiredTestFiles.push(testFile)
+      console.log("Checking test file:", testFile)
+      console.log("Impacted source files:", tia[testFile])
+      if (tia[testFile].includes(sourceFile)) {
+        console.log("test file includes source file!")
+        requiredTestFiles.push(testFile)
+      }
     }
   }
 
